39e4af1c51cd43d68a907bb50f60b7deaaa8e082
gudduabhinav <abhinavguddumtech@gmail.com>
2026-01-03 19:59:05 +0530

    Unify dashboard layout navigation

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, BottomNav } from "@/components/common/navigation";
+import { Navigation } from "@/components/common/navigation";
 
 export default function DashboardLayout({
   children,
@@ -7,12 +7,11 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="min-h-screen flex bg-slate-50 dark:bg-slate-950">
-      <Sidebar />
+      <Navigation />
       <div className="flex-1 flex flex-col min-w-0">
         <main className="flex-1 pb-24 md:pb-8 pt-6 px-4 md:px-8">
           {children}
         </main>
-        <BottomNav />
       </div>
     </div>
   );
